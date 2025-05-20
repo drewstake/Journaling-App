39a375dcf3add5e4b75d8b977ae13eadb334858c
drewstake <ajn5605@psu.edu>
2025-05-20 15:22:03 -0400

    Save all current changes

--- journalproj/urls.py
+++ journalproj/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', include('django.contrib.auth.urls')),      # keep login, password URLs
+    path('accounts/logout/',                                         
+         LogoutView.as_view(next_page='home'),                      # ← override here
+         name='logout'),
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
