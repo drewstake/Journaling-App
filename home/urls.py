888f23d12c7b11aff57b08504298c16688b78b17
drewstake <ajn5605@psu.edu>
2025-05-21 15:52:18 -0400

    Remove all files

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, signup, entry_create
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('signup/', signup, name='signup'),
-    path('create/', entry_create, name='entry_create'),
-]
